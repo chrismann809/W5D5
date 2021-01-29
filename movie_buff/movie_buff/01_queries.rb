11ee8e5b2fd43cf3ede82fa9e55c2c6341768d0c
jas-singh-code <jaspreetsingh7798@gmail.com>
2021-01-29 14:42:02 -0500

    finished #buggest_cast and #harr_ford

--- movie_buff/movie_buff/01_queries.rb
+++ movie_buff/movie_buff/01_queries.rb
@@ -28,7 +28,9 @@ def harrison_ford
     .select(:id, :title)
     .joins(:actors)
     .joins(:castings)
-    .where(:actors.name: 'Harrison Ford')
+    .where("actors.name = 'Harrison Ford' AND castings.ord > 1")
+    .uniq
+
 end
 
 def biggest_cast
@@ -46,6 +48,12 @@ def biggest_cast
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
 
+  Movie
+    .select(:id, :title)
+    .joins(:castings)
+    .group("movies.id, movies.title")
+    .order('COUNT(castings.actor_id) DESC')
+    .limit(3)
 end
 
 def directed_by_one_of(them)
